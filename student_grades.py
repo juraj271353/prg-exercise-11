e4332cf58f9d8c07b110029fee477ab39aef7ed7
juraj271353 <271353@vutbr.cz>
2026-04-23 16:07:36 +0200

    commit5

--- student_grades.py
+++ student_grades.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class StudentsGrades:
     def __init__(self, scores):
         self.scores = scores
@@ -8,6 +10,52 @@ class StudentsGrades:
     def count(self):
         return len(self.scores)
 
-grades = StudentsGrades([10,25,60,55,99])
-print(grades.get_by_index(2))
-print(grades.count())
+    def get_grade(self, index):
+        points = self.scores[index]
+
+        if points >= 90:
+            return "A"
+        elif points >= 80:
+            return "B"
+        elif points >= 70:
+            return "C"
+        elif points >= 60:
+            return "D"
+        elif points >= 50:
+            return "E"
+        else:
+            return "F"
+
+
+    def find(self,value):
+        results = []
+
+        for i in range(len(self.scores)):
+            if self.scores[i] == value:
+                results.append(i)
+
+        return results
+
+
+def main():
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+    print(results.count())  # 9
+    print(results.get_by_index(2))  # 91
+    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+
+    print(results.get_grade(2))  # A (91 bodů)
+    print(results.get_grade(6))  # A (100 bodů)
+    print(results.get_grade(7))  # F (38 bodů)
+
+    values = results.scores
+    plt.hist(values, bins=5)
+    plt.show()
+
+    print(results.find(100))  # [6]
+    print(results.find(50))  # [4]
+    print(results.find(77))  # []
+
+
+if __name__ == "__main__":
+    main()
