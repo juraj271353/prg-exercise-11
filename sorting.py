07c0d9e5989bf326a88aa85c7066acf876d3f627
juraj271353 <271353@vutbr.cz>
2026-04-23 15:13:39 +0200

    commit2

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -20,22 +21,38 @@ def selection_sort(numbers):
     return numbers
 
 
-def bubble_sort(numbers):
-    numbers = numbers.copy()
+def bubble_sort(values):
+    numbers = values.copy()
     n = len(numbers)
+
+    plt.ion()
+    plt.show()
 
     for i in range(n):
         for j in range(0, n - i - 1):
+
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
             if numbers[j] > numbers[j + 1]:
                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+    plt.ioff()
+    plt.show()
 
     return numbers
 
 
 
-def main():
-    print(selection_sort([5, 1, 4, 2, 8]))
-    print(selection_sort(random_numbers(20)))
+print(selection_sort([5, 1, 4, 2, 8]))
+print(selection_sort(random_numbers(20)))
 
-    print(bubble_sort([5, 1, 4, 2, 8]))
-    print(bubble_sort(random_numbers(20)))
+print(bubble_sort([5, 1, 4, 2, 8]))
+print(bubble_sort(random_numbers(20)))
